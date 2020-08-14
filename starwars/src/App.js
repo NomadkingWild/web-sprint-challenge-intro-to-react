9f550194c64fe1ad208d67fd7fe9030385e655ba
NomadkingWild <67304919+NomadkingWild@users.noreply.github.com>
2020-08-14 13:15:29 -0400

    It shoould work once i get rid of this rror that i dont understand.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import {BASE_URL, API_KEY} from './constants'
+import Character from './components/Character'
+import Details from './components/Details'
 import './App.css';
 
-const App = () => {
+export default function App () {
+  const[character, setCharacter]= useState([]);
+  const[currentCharacter, setCurrentCharacter]= useState('1')
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const openDetails = id =>{
+  setCurrentCharacter(id);
+}
+const closeDetails= () =>{
+  setCurrentCharacter(null)
+}
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+useEffect(()=>{
+  axios.get(`${BASE_URL}/characters?api_key=${API_KEY}`)
+  .then(res=>{
+    setCharacter(res.data)
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+  
+}, [])
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+      <h1 className="Header">Character</h1>
+      {
+        character.map((fr, idx)=>{
+          return <Character big={idx % 2 === 0} key={fr.id}info={fr} action={openDetails} actionText='see details' />
+        
+      })
+    }
+      {
+        currentCharacter && <Character characterId={currentCharacter} close={closeDetails} />
+      }
+      </div>
+
   );
 }
 
-export default App;
+
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,37 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledCharacter = styled.div`
+width: 80%;
+display:flex;
+justify-content:space-evenly;
+color: ${props => props.color || props.theme.primaryColor};
+margin: ${props =>props.theme.margin.small};
+font-size: ${props=props.big ? '1rem':'0.5rem'};
+@media (max-width: ${props=>props.theme.tabletBreakpoint}){
+    width:100%100%;
+}
+button {
+background-color:${props => props.theme.secondaryColor};
+color: ${props=>{
+    const {theme} = props;
+    return theme.white;
+}};
+    transition: all 0.5s ease-in-out;
+&:hover{
+    transition: all 0.5s ease-in-out;
+    transform: scale()(2.0);
+    }
+}
+`
+export default function Character({big, info, action, actionText}){
+    return(
+        <StyledCharacter big={big}>
+            {info.name}
+            <button onClick={()=> action(info.id)}>
+                {actionText}
+            </button>
+        </StyledCharacter>
+    )
+}
